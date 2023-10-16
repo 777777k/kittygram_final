d3ddc81a25ea42c216f149c158f829f58b223bad
Дмитрий Крупин <thediman23@gmail.com>
2023-10-16 17:56:36 +0300

    SECRET_KEY fix

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -4,7 +4,7 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY', 'default_SECRET_KEY')
 
 DEBUG = os.environ.get('DEBUG', 'False').lower() == 'true'
 
